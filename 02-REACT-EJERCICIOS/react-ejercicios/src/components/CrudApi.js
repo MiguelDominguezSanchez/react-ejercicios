09e7882bf1db3a6f66b18f6f42a02319fe8e5438
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-17 19:31:15 +0200

    35. CRUD API: Implementando Loaders y Mensajes de errores (4/5)

--- 02-REACT-EJERCICIOS/react-ejercicios/src/components/CrudApi.js
+++ 02-REACT-EJERCICIOS/react-ejercicios/src/components/CrudApi.js
@@ -3,24 +3,35 @@ import { Fragment } from 'react'
 import CrudForm from './CrudForm'
 import CrudTable from './CrudTable'
 import { helpHttp } from '../helpers/helpHttp'
+import Loader from './Loader'
+import Message from './Message'
 
 const CrudApi = () => {
-	const [db, setDb] = useState([])
+	const [db, setDb] = useState(null)
 	const [dataToEdit, setDataToEdit] = useState(null)
+	const [error, setError] = useState(null)
+	const [loading, setLoading] = useState(false)
 
 	let api = helpHttp()
 	let url = 'http://localhost:5000/santos'
 
 	useEffect(() => {
-		api.get(url).then((res) => {
-			// console.log(res)
-			if (!res.err) {
-				setDb(res)
-			} else {
-				setDb(null)
-			}
-		})
-	}, [])
+		setLoading(true)
+		helpHttp()
+			.get(url)
+			.then((res) => {
+				console.log(res)
+				if (!res.err) {
+					setDb(res)
+					setError(null)
+				} else {
+					setDb(null)
+					setError(res)
+				}
+
+				setLoading(false)
+			})
+	}, [url])
 
 	const createData = (data) => {
 		data.id = Date.now()
@@ -54,11 +65,20 @@ const CrudApi = () => {
 					dataToEdit={dataToEdit}
 					setDataToEdit={setDataToEdit}
 				/>
-				<CrudTable
-					data={db}
-					setDataToEdit={setDataToEdit}
-					deleteData={deleteData}
-				/>
+				{loading && <Loader />}
+				{error && (
+					<Message
+						msg={`Error ${error.status}: ${error.statusText}`}
+						bgColor='#dc3545'
+					/>
+				)}
+				{db && (
+					<CrudTable
+						data={db}
+						setDataToEdit={setDataToEdit}
+						deleteData={deleteData}
+					/>
+				)}
 			</article>
 		</Fragment>
 	)
