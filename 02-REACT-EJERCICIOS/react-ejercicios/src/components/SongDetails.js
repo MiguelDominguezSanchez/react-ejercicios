71f9f967726cf01384e31a01b7e33d22900f9434
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-18 12:41:24 +0200

    40. Buscador de Canciones: renderizado condicional de datos (4/5)

--- 02-REACT-EJERCICIOS/react-ejercicios/src/components/SongDetails.js
+++ 02-REACT-EJERCICIOS/react-ejercicios/src/components/SongDetails.js
@@ -1,13 +1,29 @@
+import { Fragment } from 'react'
+import Message from './Message'
 import SongArtist from './SongArtist'
 import SongLyric from './SongLyric'
 
 const SongDetails = ({ search, lyric, bio }) => {
+	if (!lyric || !bio) return null
 	return (
-		<div>
-			<h2>Detalles</h2>
-			<SongArtist />
-			<SongLyric />
-		</div>
+		<Fragment>
+			{lyric.error || lyric.err || 'AbortError' ? (
+				<Message
+					msg={`Error: no existe la canción '${search.song}'`}
+					bgColor='#dc3545'
+				/>
+			) : (
+				<SongLyric />
+			)}
+			{bio.artists ? (
+				<SongArtist />
+			) : (
+				<Message
+					msg={`Error: no existe el  interprete '${search.artist}'`}
+					bgColor='#dc3545'
+				/>
+			)}
+		</Fragment>
 	)
 }
 
--- 02-REACT-EJERCICIOS/react-ejercicios/src/components/SongSearch.js
+++ 02-REACT-EJERCICIOS/react-ejercicios/src/components/SongSearch.js
@@ -47,7 +47,9 @@ const SongSearch = () => {
 			<h2>Song Search</h2>
 			{loading && <Loader />}
 			<SongForm handleSearch={handleSearch} />
-			<SongDetails search={search} lyric={lyric} bio={bio} />
+			{search && !loading && (
+				<SongDetails search={search} lyric={lyric} bio={bio} />
+			)}
 		</div>
 	)
 }
