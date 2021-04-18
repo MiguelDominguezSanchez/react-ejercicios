61ee664ffaae9c7d243c889a52a8e143b2bc46a5
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-18 10:12:32 +0200

    37. Buscador de canciones: Definición de componentes y lógica (1/5)

--- 02-REACT-EJERCICIOS/react-ejercicios/src/App.js
+++ 02-REACT-EJERCICIOS/react-ejercicios/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import CrudApi from './components/CrudApi'
 import CrudApp from './components/CrudApp'
+import SongSearch from './components/SongSearch'
 
 function App() {
 	return (
 		<div>
 			<h1>Ejercicios con React</h1>
 			<hr />
+			<SongSearch />
+			<hr />
 			<CrudApi />
+			<hr />
 			<CrudApp />
 			<hr />
 		</div>
