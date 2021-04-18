e892ef06a6df116b4f168a4911540676eeee5d33
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-04-18 09:34:43 +0200

    36. CRUD API: Peticiones HTTP a la API (5/5)

--- 02-REACT-EJERCICIOS/react-ejercicios/src/components/CrudTable.js
+++ 02-REACT-EJERCICIOS/react-ejercicios/src/components/CrudTable.js
@@ -14,12 +14,8 @@ const CrudTable = ({ data, setDataToEdit, deleteData }) => {
 					</tr>
 				</thead>
 				<tbody>
-					{data.length === 0 ? (
-						<tr>
-							<td colSpan='3'>Sin datos</td>
-						</tr>
-					) : (
-						data.map(el => (
+					{data.length > 0 ? (
+						data.map((el) => (
 							<CrudTableRow
 								key={el.id}
 								el={el}
@@ -27,6 +23,10 @@ const CrudTable = ({ data, setDataToEdit, deleteData }) => {
 								deleteData={deleteData}
 							/>
 						))
+					) : (
+						<tr>
+							<td colSpan='3'>Sin datos</td>
+						</tr>
 					)}
 				</tbody>
 			</table>
