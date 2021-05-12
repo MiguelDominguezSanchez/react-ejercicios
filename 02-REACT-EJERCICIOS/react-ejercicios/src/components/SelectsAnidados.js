702026af1b395099b1baa1bc19848f86c019d4c7
MiguelDominguezSanchez <0.miguel.dominguez@gmail.com>
2021-05-12 11:37:06 +0200

    44. Selects Anidados: Renderizado de datos y estilos CSS (3/3)

--- 02-REACT-EJERCICIOS/react-ejercicios/src/components/SelectsAnidados.js
+++ 02-REACT-EJERCICIOS/react-ejercicios/src/components/SelectsAnidados.js
@@ -5,13 +5,16 @@ const SelectsAnidados = () => {
 	const [state, setState] = useState('')
 	const [town, setTown] = useState('')
 	const [suburb, setSuburb] = useState('')
+
+	const TOKEN = '7ff882d9-9074-4aaf-9b33-5f3be3b42e67'
+
 	return (
 		<div>
 			<h2>Selects Anidados</h2>
 			<h3>México</h3>
 			<SelectList
 				title='estados'
-				url=''
+				url={`https://api-sepomex.hckdrk.mx/query/get_estados?token=${TOKEN}`}
 				handleChange={(e) => {
 					setState(e.target.value)
 				}}
